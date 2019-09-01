7c54eeccad2c246b9d070d472786047d1031d130
Bastin Robins .J <bastinrobins@gmail.com>
2019-09-01 23:26:49 +0530

    Added: Production config for POSTGreSQL

--- app/config.py
+++ app/config.py
@@ -14,6 +14,12 @@ class Config(object):
 
 class ProductionConfig(Config):
     DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = 'postgres://qmfeisyxjvullw:423c7a964f8419b2a0fc8ff5a9fb155ff423de84f690a3fb1d2bc100437385cb@ec2-54-225-129-101.compute-1.amazonaws.com:5432/d71mtfdqmmavap'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    UPLOAD_FOLDER = 'app/uploads'
 
 
 class StagingConfig(Config):
